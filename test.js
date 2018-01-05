dd3f1e48bcb3937a69a14af319e17a55f3ed5111
Noah Schwarz <noah.anabiik.schwarz@gmail.com>
2018-01-05 14:29:55 +0100

    some tests

--- test.js
+++ test.js
@@ -1,12 +1,25 @@
 var tape = require('tape')
 
-var bits2octets = require('./index')
+var bits2bytes = require('./index')
 
-tape('bits2octets', function (t) {
+tape('bits2bytes', function (t) {
 
-  var octet = bits2octets('101')
+  t.is(bits2bytes('00100100'), '24', '36 forever')
+  
+  t.end()
+})
 
-  t.is(octet, '05', 'hex encoded string octet')
+tape('multiple bytes', function (t) {
+
+  t.is(bits2bytes('0010010000101100'), '242c')
+
+  t.end()
+})
+
+tape('padding', function (t) {
+
+  t.is(bits2bytes('101'), '05', 'got padded')
+  t.is(bits2bytes('00000101'), '05', 'hex encoded byte')
 
   t.end()
 })
